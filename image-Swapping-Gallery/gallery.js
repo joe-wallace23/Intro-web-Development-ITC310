6615e24359f6c466e0cea59f75756e104cd1a3cc
Joseph  Wallace <joewallace3005@gmail.com>
2021-11-06 19:02:45 -0500

    Gallery

---
+++ image-Swapping-Gallery/gallery.js
@@ -0,0 +1,26 @@
+// const current = document.querySelector('#current');
+// const imgs = document.querySelectorAll('.imgs img');
+const opacity= 0.6;
+const [current, imgs] = [document.querySelector('#current'), document.querySelectorAll('.imgs img') ]
+
+imgs.forEach(img => img.addEventListener('dblclick', 
+imgClick));
+
+imgs[0].style.opacity = opacity;
+
+
+function imgClick(e){
+    //
+    imgs.forEach(img => (img.style.opacity = 1));
+    // Change your images
+    current.src= e.target.src;
+
+    //add class
+    current.classList.add('fade-in');
+
+    // Remove class
+    setTimeout(() => current.classList.remove('fade-in'), 300)
+
+    // make the image lighter
+    e.target.style.opacity = opacity
+}
