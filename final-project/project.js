837cb2668e31657e02d26461b9ac11d639353cd3
Joseph  Wallace <joewallace3005@gmail.com>
2021-12-07 20:31:26 -0600

    king

--- final-project/project.js
+++ final-project/project.js
@@ -1,10 +1,20 @@
-let cars = document.querySelector(".carsTwo")
+let cars1 = document.getElementById("carsOne")
+let cars2 = document.getElementById("carsTwo")
+let cars3 = document.getElementById("carsThree")
 
-function time (){
-    
+function component(x){
+    this.speedX=20
+    this.x= x
 }
 
-cars.addEventListener('click', function(e){
-    time
+function time (this){
+    this.x += this.speedX;
+    this.speedX.cars1;
+    this.speedX.cars2;
+    this.speedX.cars2;
+}
+
+document.addEventListener('keydown', function(e){
+
 })
 
